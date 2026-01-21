0025ccc46e8a1b6e9362bca06a55c0ec73c49080
michallaskovic <michal.laskovic@icloud.com>
2026-01-21 16:40:13 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,16 +2,16 @@
 import { initializeApp, getApps, type FirebaseApp } from "@firebase/app";
 import { getAuth, type Auth } from "@firebase/auth";
 import { getFirestore, type Firestore } from "@firebase/firestore";
-import { getDatabase, type Database } from "@firebase/database"; // ✅ Realtime DB import
+import { getDatabase, type Database } from "@firebase/database"; // 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBPwpPI2Dr_0ZIEcAmSl-ypDu8YdjfXueI",
-    authDomain: "partygame-72ba0.firebaseapp.com",
-    projectId: "partygame-72ba0",
-    storageBucket: "partygame-72ba0.firebasestorage.app",
-    messagingSenderId: "523089801881",
-    appId: "1:523089801881:web:32737608ec37def316be08",
-    databaseURL: "https://partygame-72ba0-default-rtdb.europe-west1.firebasedatabase.app" // ✅ Adj hozza, ha nincs
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ",
+    databaseURL: "" // 
 };
 
 // Singleton instances
@@ -23,7 +23,7 @@ export const getFirebaseServices = () => {
         const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
         const auth = getAuth(app);
         const db = getFirestore(app);
-        const realtime = getDatabase(app); // ✅ Realtime DB init
+        const realtime = getDatabase(app); 
         services = { app, auth, db, realtime };
     }
     return services;
